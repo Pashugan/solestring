5987d54d4a49942f870b0d6f53e3863acf3cedc9
Pavel Knoblokh <pavel.knoblokh@citrusad.com>
2021-02-21 19:43:01 +1000

    Disable pointer tagging

--- solestring.go
+++ solestring.go
@@ -1,8 +1,6 @@
 package solestring
 
-// Remove -DSOLESTRING_PACK to disable pointer tagging (slightly faster but less memory-efficient)
-
-// #cgo CFLAGS: -std=c99 -g -O2 -Wall -Wpedantic -Wno-unused-variable -Itidwall_hashmap -DSOLESTRING_PACK
+// #cgo CFLAGS: -std=c99 -g -O2 -Wall -Wpedantic -Wno-unused-variable -Itidwall_hashmap
 // #include <stdlib.h>
 // #include "solestring.h"
 import "C"
